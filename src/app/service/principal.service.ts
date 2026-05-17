ebc61cdaaf0f15f28d839fb70bdb166e1ff213fe
Edson Kennis dos Santos Junior <edson.k.santos@outlook.com>
2026-05-17 18:00:36 -0300

    feat: adicionando serviços de frete

--- src/app/service/principal.service.ts
+++ src/app/service/principal.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Frete } from '../interfaces/frete';
 
 @Injectable({
   providedIn: 'root'
@@ -14,33 +15,44 @@ export class PrincipalSerivice {
   private readonly baseUrl = 'https://localhost:44384/';
 
   private addHeaders() {
-      const headers = new HttpHeaders({
-        'Content-Type': 'application/json',
-       
-      });
-  
-      // tslint:disable-next-line: object-literal-shorthand
-      const options = { headers: headers };
-  
-      return options;
-    }
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
 
-     buscarCategorias(): Observable<any> {
-      return this.http.get<any>(`${this.baseUrl}${this.API_BLING}/categorias`, {
-        ...this.addHeaders()
-      });
-    }
+    });
 
-    buscarProdutosCategoria(id: string | null): Observable<any> {
-      return this.http.get<any>(`${this.baseUrl}${this.API_BLING}/produtos/categoria/${id}`, {
-        ...this.addHeaders()
-      });
-    }
+    // tslint:disable-next-line: object-literal-shorthand
+    const options = { headers: headers };
 
-    buscarCep(cep: string): Observable<any> {
-      return this.http.get<any>(`${this.baseUrl}${this.API_BLING}/cep/${cep}`, {
-        ...this.addHeaders()
-      });
-    }
-    
+    return options;
+  }
+
+  buscarCategorias(): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}${this.API_BLING}/categorias`, {
+      ...this.addHeaders()
+    });
+  }
+
+  buscarProdutosCategoria(id: string | null): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}${this.API_BLING}/produtos/categoria/${id}`, {
+      ...this.addHeaders()
+    });
+  }
+
+  buscarCep(cep: string): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}${this.API_BLING}/cep/${cep}`, {
+      ...this.addHeaders()
+    });
+  }
+  cadastrarFrete(frete: Frete): Observable<any> {
+    return this.http.post<any>(
+      `${this.baseUrl}${this.API_BLING}/fretes`, frete, {
+      ...this.addHeaders()
+    });
+  }
+  listarFretes(): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}${this.API_BLING}/fretes`, {
+      ...this.addHeaders()
+    });
+  }
+
 }
